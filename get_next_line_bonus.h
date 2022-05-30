750944a4449cc847854dcf02a10b08d6ee06d54e
kfujita <kfujita@student.42tokyo.jp>
2022-05-31 00:54:33 +0900

    Create Bonus files
    実装はまだ

---
+++ get_next_line_bonus.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/31 00:53:46 by kfujita           #+#    #+#             */
+/*   Updated: 2022/05/31 00:54:05 by kfujita          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+// dependency:
+// - ssize_t
+# include <sys/types.h>
+
+// dependency:
+// - NULL
+# include <stddef.h>
+
+typedef struct s_str {
+	char	*m;
+	ssize_t	l;
+	ssize_t	cap;
+}	t_str;
+
+static const t_str	g_STR_ZERO = {NULL, 0, 0};
+static const t_str	g_STR_BUF = {NULL, 0, BUFFER_SIZE};
+
+char	*get_next_line(int fd);
+
+ssize_t	t_str_strchr(const t_str *str, char c);
+t_str	t_str_dup_concat(const t_str *base, const t_str *back, ssize_t backlen);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/31 00:53:38 by kfujita           #+#    #+#             */
+/*   Updated: 2022/05/31 01:01:24 by kfujita          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// dependency:
+// - NULL
+#include <stddef.h>
+
+// dependency:
+// - malloc
+// - free
+#include <stdlib.h>
+
+#include "get_next_line.h"
+
+ssize_t	t_str_strchr(const t_str *str, char c)
+{
+	ssize_t	i;
+
+	if (str == NULL || str->m == NULL || str->l <= 0)
+		return (-1);
+	i = 0;
+	while (i < str->l && str->m[i] != c)
+		i++;
+	if (i == str->l)
+		return (-1);
+	else
+		return (i);
+}
+
+t_str	t_str_dup_concat(const t_str *front, const t_str *back, ssize_t backlen)
+{
+	t_str	ret;
+
+	ret = g_STR_ZERO;
+	ret.m = malloc(front->l + backlen + 1);
+	if (ret.m == NULL)
+		return (ret);
+	while (ret.l < front->l)
+	{
+		ret.m[ret.l] = front->m[ret.l];
+		ret.l++;
+	}
+	while ((ret.l - front->l) < backlen)
+	{
+		ret.m[ret.l] = back->m[ret.l - front->l];
+		ret.l++;
+	}
+	ret.m[ret.l] = '\0';
+	return (ret);
+}
