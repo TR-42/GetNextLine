81744bfb137c3f339ed4666461da471779a57f63
kfujita <kfujita@student.42tokyo.jp>
2022-05-31 00:51:04 +0900

    Complete mandatory

---
+++ get_next_line.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/28 10:51:51 by kfujita           #+#    #+#             */
+/*   Updated: 2022/05/31 00:50:19 by kfujita          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// dependency:
+// - malloc
+// - free
+#include <stdlib.h>
+
+// dependency:
+// - read
+#include <unistd.h>
+
+// dependency:
+// - NULL
+#include <stddef.h>
+
+// dependency:
+// - bool
+// - true
+// - false
+#include <stdbool.h>
+
+#include "get_next_line.h"
+
+// 1. Get length (named `len`) up to a newline from buf
+// 2. Copy&paste/concat the contents of ret&buf into the additional malloced mem
+// 3. Free the original memory of ret and replace it with the newly acquired one
+static bool	is_newline_picked_from_buf(t_str *buf, t_str *ret)
+{
+	t_str	new_ret;
+	ssize_t	lf_index;
+	ssize_t	i;
+
+	lf_index = t_str_strchr(buf, '\n');
+	if (lf_index >= 0)
+	{
+		new_ret = t_str_dup_concat(ret, buf, lf_index + 1);
+		free(ret->m);
+		*ret = new_ret;
+		if (ret->m == NULL)
+			return (true);
+		i = 0;
+		while ((++i + lf_index) < buf->l)
+			buf->m[i - 1] = buf->m[i + lf_index];
+		buf->l -= lf_index + 1;
+		return (true);
+	}
+	new_ret = t_str_dup_concat(ret, buf, buf->l);
+	free(ret->m);
+	*ret = new_ret;
+	if (ret->m == NULL)
+		return (true);
+	buf->l = 0;
+	return (false);
+}
+
+// return: is successed
+static bool	init_static_var(t_str *buf)
+{
+	size_t			i;
+
+	if (buf->m != NULL)
+		return (true);
+	buf->m = malloc(buf->cap);
+	if (buf->m == NULL)
+		return (false);
+	i = 0;
+	while (i < (size_t)buf->cap)
+		buf->m[i++] = '\0';
+	return (true);
+}
+
+char	*get_next_line(int fd)
+{
+	static t_str	buf = {NULL, 0, BUFFER_SIZE};
+	t_str			ret;
+
+	ret = g_STR_ZERO;
+	if (init_static_var(&buf) != true)
+		return (NULL);
+	if (buf.l > 0 && is_newline_picked_from_buf(&buf, &ret))
+		return (ret.m);
+	while (true)
+	{
+		buf.l = read(fd, buf.m, buf.cap);
+		if (buf.l <= 0)
+		{
+			free(buf.m);
+			buf = g_STR_BUF;
+			break ;
+		}
+		if (is_newline_picked_from_buf(&buf, &ret) == true)
+			break ;
+	}
+	return (ret.m);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,33 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 10:53:01 by kfujita           #+#    #+#             */
-/*   Updated: 2022/05/30 22:34:30 by kfujita          ###   ########.fr       */
+/*   Updated: 2022/05/31 00:49:44 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+// dependency:
+// - ssize_t
+# include <sys/types.h>
+
+// dependency:
+// - NULL
+# include <stddef.h>
+
+typedef struct s_str {
+	char	*m;
+	ssize_t	l;
+	ssize_t	cap;
+}	t_str;
+
+static const t_str	g_STR_ZERO = {NULL, 0, 0};
+static const t_str	g_STR_BUF = {NULL, 0, BUFFER_SIZE};
+
 char	*get_next_line(int fd);
+
+ssize_t	t_str_strchr(const t_str *str, char c);
+t_str	t_str_dup_concat(const t_str *base, const t_str *back, ssize_t backlen);
 
 #endif
